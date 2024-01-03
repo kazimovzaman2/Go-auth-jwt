9c5c20e44d28c0d24d9dbca06e54dddb042db14e
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-01-03 09:31:23 +0400

    Models and database connection done

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/kazimovzaman2/auth-jwt/database"
 )
 
 func main() {
 	app := fiber.New()
 	app.Use(cors.New())
+
+	database.ConnectDB()
 }
